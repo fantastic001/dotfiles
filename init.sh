d014ed2ec0d155c0d2f331b6cd8d1dfa0da3c277
Stefan Nožinić <stefan@lugons.org>
2016-08-07 16:34:49 +0200

    Fix bug in init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 cp xinitrc ~/.xinitrc
-cp -r awesome ~/.config/awesome
-cp -r vim ~/.vim 
+rm -rf ~/.vim/
+cp -r awesome/ ~/.config/
+cp -r vim/ ~/.vim
 cp -r vimrc ~/.vimrc
 cp Xdefaults ~/.Xdefaults
 cp xbindkeys ~/.xbindkeysrc
 cp xmodmap ~/.Xmodmap 
 
-cp -r i3 ~/.config/
+cp -r i3/ ~/.config/
